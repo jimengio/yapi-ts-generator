cce17b842da2d1e78067ecb6a32e2c77422069ab
ChenYong <chenyong@jimeng.io>
2020-05-29 18:29:43 +0800

    refactor: purge unused page code in demo

--- example/models/router-rules.ts
+++
@@ -1,3 +0,0 @@
-import { IRouteRule } from "@jimengio/ruled-router";
-
-export const routerRules: IRouteRule[] = [{ path: "home" }, { path: "", name: "home" }];
--- webpack/dll-release.js
+++ webpack/dll-release.js
@@ -8,7 +8,7 @@ let { matchCssRule, matchFontsRule } = require("./shared");
 
 module.exports = {
   mode: "production",
-  entry: ["react", "react-dom", "emotion", "immer", "lodash"],
+  entry: ["react", "lodash"],
   output: {
     filename: "dll_vendors_[hash:8].js",
     path: path.join(__dirname, "../dist"),
